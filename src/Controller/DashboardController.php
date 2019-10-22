c6a69c2614eaa8b098057c226ff6263680bc0297
Paul Steven <pwsteven13@gmail.com>
2019-10-22 16:24:44 +0100

    Dashboard build

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,10 +3,9 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractController
+class DashboardController extends BaseController
 {
     /**
      * @Route("/dashboard", name="app_dashboard")
@@ -15,7 +14,7 @@ class DashboardController extends AbstractController
     public function index()
     {
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+
         ]);
     }
 }
